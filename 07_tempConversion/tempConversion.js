0a631fb669d4a9690e49b428709c511576cda311
Taha Munawar <109829503+tahamunawar@users.noreply.github.com>
2025-01-31 19:42:03 +0500

    Complete JS Arrays and Loops

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let ans = (tempF-32)*5/9;
+  return Math.round(ans*10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let ans = tempC*9/5+32;
+  return Math.round(ans*10)/10;
 };
 
 // Do not edit below this line
